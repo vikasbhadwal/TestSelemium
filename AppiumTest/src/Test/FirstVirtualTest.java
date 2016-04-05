67b9532baeea780b0cbcd647dd8e20d350242b4c
Vikas Bhadwal <vbhadwal@tk20.com>
2016-04-05 14:48:49 +0530

    nvhn

--- AppiumTest/src/Test/FirstVirtualTest.java
+++ AppiumTest/src/Test/FirstVirtualTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By; import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test; 
-
+//check pusshhhhhhhhhh
 public class FirstVirtualTest {
 	WebDriver driver;
 	@Test public void setUp() throws Exception {
